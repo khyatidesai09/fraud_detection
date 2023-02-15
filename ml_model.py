ca3250bc59a9bf7bec71e6476472ad78adadafe7
khyatidesai09 <kd91826@gmail.com>
2023-02-15 13:53:56 +0530

    changed model 1 and 2 to creat conflict.

--- ml_model.py
+++ ml_model.py
@@ -3,12 +3,12 @@ print("adding all model related code.")
 
 
 def model1():
-    print("Adding code for model 1")
+    print("Adding code for model 1. changed model 1 diff line")
 
 def model2():
-    print("Adding code for model 2. This is working best. changed to create conflict. New change in the same line")
+    print("Adding code for model 2. This is working best. changed to create conflict. New change in the same line 2")
 
 def model3():
-    print("Adding code for model 3. New change in diff line 3")
+    print("Adding code for model 3.")
 
 print("Model 2 is performing best. changing to create conlict.")
