0746dc5aebd53d632706389fb841955948dfe22e
khyatidesai09 <kd91826@gmail.com>
2023-02-15 13:33:35 +0530

    added 3 new model codes

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("adding all model related code.")
+
+
+
+def model1():
+    print("Adding code for model 1")
+
+def model2():
+    print("Adding code for model 2")
+
+def model3():
+    print("Adding code for model 3")
